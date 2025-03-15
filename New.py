2e5347fc43704b464e8c9b28b391b6e58fa4e89b
Sansui-plus <3302953410@qq.com>
2025-03-15 16:25:30 +0800

    Update New.py

--- New.py
+++ New.py
@@ -1,7 +1,7 @@
-from ollama import chat
-from ollama import ChatResponse
+# from ollama import chat
+# from ollama import ChatResponse
 
-Text = []
+#Text = []
 
 
 # response: ChatResponse = chat(model='gemma2:2b', messages=[
@@ -18,18 +18,23 @@ Text = []
 
 # print(response['message']['content'])
 
-def confirmation(Text):
-    response: ChatResponse = chat(model='gemma2:2b', messages=[
-        {
-            'role': 'system',
-            'content': '這裏有一些人員名和待辦事宜的清單，請從以下文字内容中找到對應人員和相應事宜，如果找不到對應的文字内容，請以回復N/A'
-        },
-        {
+# def confirmation(Text):
+#     response: ChatResponse = chat(model='gemma2:2b', messages=[
+#         {
+#             'role': 'system',
+#             'content': '這裏有一些人員名和待辦事宜的清單，請從以下文字内容中找到對應人員和相應事宜，如果找不到對應的文字内容，請以回復N/A'
+#         },
+#         {
 
-            "role": "user",
-            "content": Text
-        }
-    ])
-    return response['message']['content']
+#             "role": "user",
+#             "content": Text
+#         }
+#     ])
+#     return response['message']['content']
 
-print(confirmation(''))
+#print(confirmation(''))
+
+
+
+
+pip install SpeechRecognition pyaudio
