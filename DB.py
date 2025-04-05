2802b49df643d911b9b240736f0500981eec0291
mr-page <84085067+mr-page@users.noreply.github.com>
2025-04-06 00:25:32 +0800

    Update DB.py

--- DB.py
+++ DB.py
@@ -12,18 +12,20 @@ Orderliness={
 }
 
 
-db  = {
+Db  = [
+    {
     'patient1':{'1:00':['Location','comment'],
                 '2:00':['Location','comment'],
 
 
+    }
     },
+        {
     'patient2':{'1:00':['Location','comment'],
                 '2:00':['Location','comment'],
-
-
-    },
-}
+    }
+        }
+]
 
 # # Create the pandas DataFrame
 # time = input('When?')
@@ -35,8 +37,11 @@ db  = {
 
 
 def time_resort(orderliness,db):
-    for patient in db.value():
-        for event in patient.value():
-            pass
-    return
+    temp_db = []
+    for patient in db:
+        for event in patient.values():
+            temp_db.append([patient.values(),event])
+    return temp_db
+print(time_resort(Orderliness,Db))
+print(time_resort(Orderliness,Db)[0])
 
