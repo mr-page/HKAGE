923baac9d5dcc7fbcea4b1d1b36a633446a90157
mr-page <84085067+mr-page@users.noreply.github.com>
2025-04-05 15:44:44 +0800

    WIP

--- fastapi/Login_api2.py
+++ fastapi/Login_api2.py
@@ -1,7 +1,7 @@
 import os
 os.system('''
-curl -X 'GET' \
-  'instantly-beloved-griffon.ngrok-free.app/test' \
+curl -X 'POST' \
+  'https://instantly-beloved-griffon.ngrok-free.app/token' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d 'grant_type=password&username=user1&password=password1&scope=&client_id=string&client_secret=string'
--- fastapi/login_api.py
+++ fastapi/login_api.py
@@ -1,6 +1,6 @@
 import requests
 data = {'grant_type':'password','username':'user1','password':'password1','scope':'','client':'string','client_secret':'string'}
-response = requests.post('http://127.0.0.1:8000/token', data)
+response = requests.post('https://instantly-beloved-griffon.ngrok-free.app/token', data)
 print(response.json()['access_token'])
 access_token = response.json()['access_token']
 
