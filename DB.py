8eda175a765821186caca1c6741c511d618548a0
JLuser-ctrl <jasperlee1509@gmail.com>
2025-04-26 14:19:06 +0800

    Comments added + fixes

--- DB.py
+++ DB.py
@@ -4,12 +4,6 @@ from datetime import  datetime
 tstamp = datetime.strptime('3:59',
                   '%H:%M')
 
-Orderliness={
-    'patient1':1,
-    'patient2':2,
-}
-
-
 Db  =  {
     'patient1':[['1:00','Location','11'],
                 ['2:00','Location','12']],
@@ -21,27 +15,34 @@ Db  =  {
         }
 
 
-#hour = print(assignment[0].split(':')[0])
-#minute = print(assignment[0].split(':')[1])
+#blank lists
 arrangement = []
 re_arrangement = []
 pa_na = []
 pa_task = []
+#Spliting the tasks out
 for patients in Db:
    pa_na.append(patients)
 for assignments in Db.values():
    pa_task.append(assignments)
 num1 = 0
 num2 = 0
+#First choose which patient
 while num1 < len(Db):
    num2 = 0
+   #Then the tasks inside the patient's list
    while num2 < len(pa_task[num1]):
+    #Rearrange the tasks into blank list as the format:[time, patient, location, guide_num]
     pa_task[num1][num2].insert(1,pa_na[num1])
     num2 += 1
    num1 += 1
+#Put stuff in arrangement (list)
 for task_list in pa_task:
    for task in task_list:
       arrangement.append(task)
+
+print(arrangement)
+"""
 for item in arrangement:
    hour = (item[0].split(':')[0])
    minute = (item[0].split(':')[1])
@@ -61,27 +62,13 @@ for time in time_list:
       arrangement.remove(arrangement[min_minute])
    else:
       re_arrangement.append(arrangement[min_hour])
-      arrangement.remove(arrangement[min_hour])
-
-   
-   
-
-
-
-   
-   
-      
-
-
-
-      
-
+      arrangement.remove(arrangement[min_hour])"""
+#To delete stuff
 def deleter(patient, time, location, message):
    info = Db[patient]
-   task_a = [time, location, message]
+   task_a = [time, patient, location, message]
    for task in info:
       if task == task_a:
         print('TAT')
         Db[patient].remove(task)
-deleter('patient2','1:30','Location','13')
-print(Db)
+deleter('patient1','2:00','Location','12')
