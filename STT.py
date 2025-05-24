d47db8c08fec0077b2ee1149c93bfeb40eff636a
mr-page <84085067+mr-page@users.noreply.github.com>
2025-05-24 16:15:20 +0800

    Update STT.py

--- STT.py
+++ STT.py
@@ -1,26 +1,33 @@
-import speech_recognition as sr
+import base64
+import requests
 
-def speech_to_text():
-    recognizer = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("请开始说话（10秒内）...")
-        
-        recognizer.adjust_for_ambient_noise(source, duration=0.5)
-        
-        try:
-            audio = recognizer.listen(source, timeout=10)
-            print("识别中...")
-            text = recognizer.recognize_google(audio, language='zh-CN')
-            print("\n识别结果：\n" + text)
 
-        except sr.WaitTimeoutError:
-            print("未检测到语音输入")
-        except sr.UnknownValueError:
-            print("无法识别语音内容")
-        except sr.RequestError as e:
-            print(f"服务请求失败；{e}")
-        except Exception as e:
-            print(f"发生未知错误：{e}")
 
-if __name__ == "__main__":
-    speech_to_text()
+def audio2text(audio_file_path):
+    with (open(audio_file_path,"rb") as audio_file):
+        audio_data = audio_file.read()
+        audio_base64 = base64.b64decode(audio_data).decode("uft-8")
+        url = "https:''text.pollinations.ai/openai"
+        prompt = "what's in this audio? Give me the text in traditional Chinese"
+        payload = {
+            "message":[
+                {
+                    "role": "user",
+                    "content" : [
+                        {"type": "text", "text" : prompt},
+                        {
+                            "type" : "input_audio",
+                            "input_audio": {
+                                "data" : audio_base64,
+                                "format" : "mp3"
+                            }
+                        }
+                    ]
+                }
+            ],
+            "model" : "openai-audio"
+        }
+        response = requests.post(url=url,json=payload)
+        return response.json()
+
+
